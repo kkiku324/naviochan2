9f7be681b9fa98f71aed883bbfacede10c1e9df4
kkiku324 <65221512+kkiku324@users.noreply.github.com>
2020-07-24 23:37:38 +0900

    first commit

---
+++ src/main/java/com/herokuapp/naviochan2/Naviochan2Application.java
@@ -0,0 +1,13 @@
+package com.herokuapp.naviochan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Naviochan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Naviochan2Application.class, args);
+	}
+
+}
