61da9975ff2beaeb324b9e6a8dce9dfdad3e1abb
kkiku324 <kkiku324@outlook.com>
2020-07-25 00:22:01 +0900

    revised to prev

--- src/main/java/com/herokuapp/naviochan2/test/TestController.java
+++ src/main/java/com/herokuapp/naviochan2/test/TestController.java
@@ -2,9 +2,14 @@ package com.herokuapp.naviochan2.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/test/")
 public class TestController {
+	
+	@GetMapping("/")
 	public String test(Model model) {
 		model.addAttribute("key", "value");
 		
