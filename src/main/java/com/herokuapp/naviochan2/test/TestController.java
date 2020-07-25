a048da494587479fa8594035f3dcb04fbbc12627
kkiku324 <kkiku324@outlook.com>
2020-07-25 12:10:00 +0900

    sql test

--- src/main/java/com/herokuapp/naviochan2/test/TestController.java
+++ src/main/java/com/herokuapp/naviochan2/test/TestController.java
@@ -1,17 +1,36 @@
 package com.herokuapp.naviochan2.test;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/")
 public class TestController {
 	
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public TestController(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+	
 	@GetMapping("/")
 	public String test(Model model) {
-		model.addAttribute("key", "value");
+		//String sql = "SELECT id, name, body FROM comments ORDER BY id DESC";
+		String sql = "SELECT id, name, body FROM comments WHERE id = 1";
+		
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+		
+		model.addAttribute("id", map.get("id"));
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("body", map.get("body"));
 		
 		return "test";
 	}
